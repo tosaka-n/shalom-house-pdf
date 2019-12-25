28d2aa8619c65ff4623e1d41143ea264bbf403a7
tosaka-n <31643545+tosaka-n@users.noreply.github.com>
2019-12-25 19:57:32 +0900

    feat: moth select (#1)
    * add: month select func
    
    * update: README, month select

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
    - pass
 3. npm i
 4. node kyuyo
+
+if you want to save other than this month pdf files, like this
+```
+node kyuyo 10
+```
--- kyuyo.js
+++ kyuyo.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 async function snap() {
+  const month =  process.argv[2] ? getMonth(process.argv[2]) : getMonth();
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   // await page.setViewport({ width: 960, height: 800 });
@@ -11,21 +12,33 @@ async function snap() {
   await page.type('#txtPsw', process.env.pass);
   await page.click("#btnLogin");
   await page.waitForSelector('#ctl00_ContentPlaceHolder1_imgBtnMeisai');
+  console.log(`login complete`);
   await page.click('#ctl00_ContentPlaceHolder1_imgBtnMeisai');
   await page.waitForSelector('#ctl00_ContentPlaceHolder1_btnSearch');
+
+  await page.select('#ctl00_ContentPlaceHolder1_cboMonth', `${month}`);
   await page.click('#ctl00_ContentPlaceHolder1_btnSearch');
+  console.log("click search button");
   await page.waitForSelector('#ctl00_ContentPlaceHolder1_btnPrint');
   await page.click('#ctl00_ContentPlaceHolder1_btnPrint');
   await page.pdf({
-    path: `${new Date().getFullYear()}${new Date().getMonth() + 1}.pdf`,
+    path: `${new Date().getFullYear()}${month}.pdf`,
     landscape: false,
     width: 960,
     height: 600,
     pageRanges: "1"
   });
+  console.log(`output: ${new Date().getFullYear()}${month}.pdf`)
   await browser.close()
 }
 
 (async () => {
   await snap();
 })().catch(e => console.error(e));
+
+function getMonth(month) {
+  if (month) {
+    return ("00" + month).substr(-2, 2);
+  }
+  return ("00" + (new Date().getMonth() + 1)).substr(-2, 2);
+}
